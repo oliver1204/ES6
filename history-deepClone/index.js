478128665c3ede68cca2f9fd0ebe880f3c504bfb
olifer <olifer655@users.noreply.github.com>
2020-12-03 15:14:44 +0800

    Update index.js

--- history-deepClone/index.js
+++ history-deepClone/index.js
@@ -8,12 +8,12 @@ function deepClone(value, hash = new WeakMap) {
 
   // 判断 该对象是否已经被拷贝过了，
   // 如果已经拷贝过了，直接返回，不再进行深拷贝，
-  // 如果没有拷贝过，继续进行
+  // 如果没有拷贝过，继续进行， 防止 obj.a = obj 这种循环引用的情况
   if(hash.has(value)) return hash.get(value);
   hash.set(value, instance);
 
-  for(let key in value) {
-    if(value.hasOwnProperty(key)) {
+  for(let key in value) { // object || array 的key都可以拿到
+    if(value.hasOwnProperty(key)) { // 只拷贝原型上的属性
       instance[key] = deepClone(value[key]);
     }
   }
